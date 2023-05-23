5b8bc30b26cf1eaaef3457fe7dc21182a5ad525d
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-05-23 11:57:51 +0200

    fixed code setup

--- BuildWeek01_be/src/controller/Main.java
+++ BuildWeek01_be/src/controller/Main.java
@@ -11,7 +11,7 @@ import DAO.CardDAO;
 public class Main {
 
 	public static void main(String[] args) {
-
+        System.out.println("Hello World!");
 	}
 
 	public static void randomSubscriptionTest() {
